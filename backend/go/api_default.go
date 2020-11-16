6902f87601119ae63f4d70c28464dc0125538d9c
lottever <lotte.verheyden@gmail.com>
2020-11-16 10:29:16 +0100

    GetSearchResultImpl implemented

--- backend/go/api_default.go
+++ backend/go/api_default.go
@@ -16,6 +16,7 @@ import (
 func GetSearchResults(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
+	GetSearchResultsImpl(w, r)
 }
 
 func GetShow(w http.ResponseWriter, r *http.Request) {
