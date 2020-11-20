a809c41b47890ea575265fab66fd6dc44e20de59
joren.sjongers <joren.sjongers@icloud.com>
2020-11-20 13:36:31 +0100

    swagger codegen

--- backend/go/api_default.go
+++ backend/go/api_default.go
@@ -13,6 +13,11 @@ import (
 	"net/http"
 )
 
+func GetBestOfGenre(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+}
+
 func GetEpisode(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
