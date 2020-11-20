43034b7d5b27f563c71c923c237f8b8fb8ac78f7
joren.sjongers <joren.sjongers@icloud.com>
2020-11-20 13:50:16 +0100

    implement route in backend

--- backend/go/api_default.go
+++ backend/go/api_default.go
@@ -16,6 +16,7 @@ import (
 func GetBestOfGenre(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
+	GetBestOfGenreImpl(w, r)
 }
 
 func GetEpisode(w http.ResponseWriter, r *http.Request) {
