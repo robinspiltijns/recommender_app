b66aabb29f366dbbea5c74912adf49f3ac91249c
joren.sjongers <joren.sjongers@icloud.com>
2020-11-22 12:51:29 +0100

    add impl

--- backend/go/api_default.go
+++ backend/go/api_default.go
@@ -16,44 +16,53 @@ import (
 func GetBestOfGenre(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
+	GetBestOfGenreImpl(w, r)
 }
 
 func GetEpisode(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
+	GetEpisodeImpl(w, r)
 }
 
 func GetEpisodeRecommendationsBasedOnEpisode(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
+	GetEpisodeRecommendationsBasedOnEpisodeImpl(w, r)
 }
 
 func GetGenres(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
+	GetGenresImpl(w, r)
 }
 
 func GetPodcastRecommendationsBasedOnPodcast(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
+	GetPodcastRecommendationsBasedOnPodcastImpl(w, r)
 }
 
 func GetSearchResults(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
+	GetSearchResultsImpl(w, r)
 }
 
 func GetShow(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
+	GetShowImpl(w, r)
 }
 
 func GetTheBestPodcasts(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
+	GetTheBestPodcastsImpl(w, r)
 }
 
 func Test(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
+	TestImpl(w, r)
 }
