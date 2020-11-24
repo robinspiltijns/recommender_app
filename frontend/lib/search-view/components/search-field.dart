22d18a42a6ac232a2e907d281dc718014a3266d9
robinspitlijns <spiltijnsrobin@gmail.com>
2020-11-24 12:21:44 +0100

    separate search field component

---
+++ frontend/lib/search-view/components/search-field.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SearchFieldWidget extends StatelessWidget {
+
+  final ValueChanged<String> onSubmit;
+
+  const SearchFieldWidget({this.onSubmit});
+
+  @override
+  Widget build(BuildContext context) {
+    return  TextField(
+      onSubmitted: onSubmit,
+      style: TextStyle(color: Colors.white),
+      cursorColor: Colors.white,
+      decoration: InputDecoration(
+        prefixIcon:
+        Icon(Icons.search, color: Colors.white.withOpacity(0.5)),
+        hintText: 'Title, description, genre, ...',
+        hintStyle: TextStyle(color: Colors.white.withOpacity(0.5)),
+        filled: true,
+        fillColor: Colors.white.withOpacity(0.1),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(25.8),
+          borderSide: BorderSide(
+            width: 0,
+            style: BorderStyle.none,
+          ),
+        ),
+        contentPadding: const EdgeInsets.only(left: 14.0, right: 14.0),
+      ),
+    );
+  }
+
+}
--- frontend/lib/search-view/search-view.dart
+++ frontend/lib/search-view/search-view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:frontend/search-view/components/search-field.dart';
 import 'package:swagger/api.dart';
 
 class SearchWidget extends StatefulWidget {
@@ -28,27 +29,7 @@ class _SearchWidgetState extends State<SearchWidget> {
         margin: const EdgeInsets.only(left: 30, right: 30),
         child: Column(
           children: [
-            TextField(
-              onSubmitted: _onSubmit,
-              style: TextStyle(color: Colors.white),
-              cursorColor: Colors.white,
-              decoration: InputDecoration(
-                prefixIcon:
-                    Icon(Icons.search, color: Colors.white.withOpacity(0.5)),
-                hintText: 'Title, description, genre, ...',
-                hintStyle: TextStyle(color: Colors.white.withOpacity(0.5)),
-                filled: true,
-                fillColor: Colors.white.withOpacity(0.1),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(25.8),
-                  borderSide: BorderSide(
-                    width: 0,
-                    style: BorderStyle.none,
-                  ),
-                ),
-                contentPadding: const EdgeInsets.only(left: 14.0, right: 14.0),
-              ),
-            ),
+            SearchFieldWidget(onSubmit: _onSubmit),
             Column(children: results.map((result) => Text(result)).toList())
           ],
         ));
