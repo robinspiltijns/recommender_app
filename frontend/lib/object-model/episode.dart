c2672078aa768f394ef7ad1e91423d89f185b44b
joren.sjongers <joren.sjongers@icloud.com>
2020-11-24 20:35:08 +0100

    add episode object model

---
+++ frontend/lib/object-model/episode.dart
@@ -0,0 +1,10 @@
+class Episode {
+
+  final String title;
+  final String id;
+  final String imageUrl;
+  final int duration; // The duration of the episode in seconds
+
+  Episode(this.title, this.id, this.imageUrl, this.duration);
+
+}
