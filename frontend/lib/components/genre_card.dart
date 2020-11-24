2db11d5ba728dccddbd99336f179a7259678eee5
joren.sjongers <joren.sjongers@icloud.com>
2020-11-24 14:52:38 +0100

    add genre card widget

---
+++ frontend/lib/components/genre_card.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/object_model/genre.dart';
+
+
+List<Color> GENRE_COLORS = [
+  Color(0xFF9BC995),
+  Color(0xFFDF2935),
+  Color(0xFFA78682),
+  Color(0xFF98B9AB),
+  Color(0xFFF1A208),
+  Color(0xFF84ACCE),
+  Color(0xFF06A77D),
+  Color(0xFFF17300),
+];
+
+
+class GenreCard extends StatelessWidget {
+
+  final Genre _genre;
+
+  GenreCard(this._genre);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+        color: getGenreColor(this._genre.getId())
+      ),
+      child: Text("bla")
+    );
+  }
+
+  Color getGenreColor(int id) {
+    return GENRE_COLORS[id % GENRE_COLORS.length];
+  }
+}
