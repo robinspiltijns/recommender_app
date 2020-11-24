f4aebc9524f8580bb7a11d9616a02990d71d4cec
joren.sjongers <joren.sjongers@icloud.com>
2020-11-24 14:52:24 +0100

    add genre object model

---
+++ frontend/lib/object_model/genre.dart
@@ -0,0 +1,16 @@
+class Genre {
+
+  Genre(this._id, this._name);
+
+  final int _id;
+  final String _name;
+
+  int getId() {
+    return this._id;
+  }
+
+  String getName() {
+    return this._name;
+  }
+
+}
