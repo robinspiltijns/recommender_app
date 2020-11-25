161621a02fdfb65f9e9e99fc957f19d52ff33474
robinspitlijns <spiltijnsrobin@gmail.com>
2020-11-25 18:31:33 +0100

    player view back button and purple background

--- frontend/lib/player-view/player-view.dart
+++ frontend/lib/player-view/player-view.dart
@@ -11,15 +11,25 @@ class _PlayerViewWidgetState extends State<PlayerViewWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xff28263B),
       appBar: AppBar(
-        title: Text("Second Route"),
+        title: IconButton(
+            icon: Icon(Icons.keyboard_arrow_down, color: Colors.white),
+            // TODO: Dit zou cleaner moeten kunnen, maar dit is kinda functioneel dus weet niet meteen hoe.
+            onPressed: () {
+              Navigator.pop(context);
+            }
+        ),
+        backgroundColor: Colors.transparent,
+        shadowColor: Colors.transparent,
+        automaticallyImplyLeading: false,
       ),
       body: Center(
         child: ElevatedButton(
           onPressed: () {
             Navigator.pop(context);
           },
-          child: Text('Go back!'),
+          child: Text("Hey"),
         ),
       ),
     );
