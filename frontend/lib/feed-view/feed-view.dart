e3628dce2e25cc6f9c5cf0eb16641788a0cbaac7
lottever <lotte.verheyden@gmail.com>
2020-11-25 12:28:22 +0100

    Dynamic section building werkt

--- frontend/lib/feed-view/feed-view.dart
+++ frontend/lib/feed-view/feed-view.dart
@@ -15,20 +15,52 @@ class _FeedWidgetState extends State<FeedWidget> {
   Future<GetPodcastRecommendationsResponse> futureResp;
 
   List<Map<String, dynamic>> sections = [
-    {"basis" : RecommendationBasis.PODCAST, "id" : "4d3fe717742d4963a85562e9f84d8c79", "sectionTitleDescription" : "Star Wars"}
+    {"basis" : RecommendationBasis.PODCAST, "id" : "4d3fe717742d4963a85562e9f84d8c79", "sectionTitleDescription" : "Star Wars"},
+    {"basis" : RecommendationBasis.PODCAST, "id" : "c463d5980b8e480fb78db6b3ed6be115", "sectionTitleDescription" : "Maed in India"}
   ];
-
-  List futures;
 
   @override
   void initState() {
     super.initState();
-    futures = sections.map((section) => {switch (section["basis"])
-    {
-    case RecommendationBasis.PODCAST: {api.getPodcastRecommendationsBasedOnPodcast(section["id"]);}
-    break;
-    }
-    }).toList();
+    makeFutures();
+  }
+
+  List<Future<Map<String, dynamic>>> futures;
+
+   makeFutures() async {
+    List<Map<String, dynamic>> result;
+    for (var index = 0; index <sections.length; index++) {
+      if (sections[index]["basis"] == RecommendationBasis.PODCAST) {
+        await AddSection(index);
+      }
+    };
+  }
+
+  AddSection(int entryIndex) async {
+     sections[entryIndex]["recommendations"] = api.getPodcastRecommendationsBasedOnPodcast(sections[entryIndex]["id"]);
+  }
+
+  List<Widget> generateSections() {
+     List<Widget> result = [];
+     for (var section in sections) {
+       result.add(FutureBuilder<GetPodcastRecommendationsResponse>(
+           future: section["recommendations"],
+           builder: (context, snapshot) {
+             if (snapshot.hasData) {
+               if (snapshot.data.runtimeType == GetPodcastRecommendationsResponse) {
+                 var res = snapshot.data as GetPodcastRecommendationsResponse;
+                 return FeedViewSection(b: section["basis"], recDescription: section["sectionTitleDescription"], resList: res.recommendations,);
+               }
+             } else if (snapshot.hasError) {
+               return Text("${snapshot.error}");
+             }
+
+             // By default, show a loading spinner.
+             return Text("loading...");
+           }
+       ));
+     }
+     return result;
   }
 
   @override
@@ -36,12 +68,17 @@ class _FeedWidgetState extends State<FeedWidget> {
     return Container(
         child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
+          children: generateSections()
+
+    /*[
           FutureBuilder<GetPodcastRecommendationsResponse>(
-          future: futureResp,
+          future: sections[0]["recommendations"],
             builder: (context, snapshot) {
               if (snapshot.hasData) {
-                return FeedViewSection(b: RecommendationBasis.PODCAST, recDescription: "This podcast", resList: snapshot.data.recommendations,);
+                if (snapshot.data.runtimeType == GetPodcastRecommendationsResponse) {
+                  var res = snapshot.data as GetPodcastRecommendationsResponse;
+                  return FeedViewSection(b: sections[0]["basis"], recDescription: sections[0]["sectionTitleDescription"], resList: res.recommendations,);
+                }
               } else if (snapshot.hasError) {
                 return Text("${snapshot.error}");
               }
@@ -50,7 +87,7 @@ class _FeedWidgetState extends State<FeedWidget> {
               return Text("loading...");
             },
           )
-          ],
+          ]*/,
         ));
   }
 }
