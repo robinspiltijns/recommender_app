25e4a6efe3a6b3ccd5d9e09fa94ca72c7d6c96c0
robinspitlijns <spiltijnsrobin@gmail.com>
2020-11-26 18:26:58 +0100

    can play and pause through service

--- frontend/lib/common/components/small-player.dart
+++ frontend/lib/common/components/small-player.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:frontend/common/services/player-service.dart';
 import 'package:frontend/player-view/player-view.dart';
+import 'package:provider/provider.dart';
 import 'package:swagger/api.dart';
 import 'package:audioplayers/audioplayers.dart';
 
@@ -17,43 +19,30 @@ class _SmallPlayerWidgetState extends State<SmallPlayerWidget> {
 
   bool isPlayingAudio = false;
 
-  _onPlayButtonPress() {
-    if (!isPlayingAudio) {
-      Future<EpisodeFull> futureEpisode =
-          api.getEpisode("02f0123246c944e289ee2bb90804e41b");
-      futureEpisode.then((episodeFull) => {
-            audioPlayer.play(episodeFull.audio).then((result) => {
-                  if (result == 1)
-                    {
-                      this.setState(() {
-                        isPlayingAudio = true;
-                      })
-                    }
-                })
-          });
+  _onPlayButtonPress(PlayerService playerService) {
+    if(playerService.isPlayingAudio) {
+      playerService.pause();
     } else {
-      audioPlayer.pause();
-      this.setState(() {
-        isPlayingAudio = false;
-      });
+      playerService.resume();
     }
   }
 
   _onOpenPlayerPress() {
     return PageRouteBuilder(
-        pageBuilder: (context, animation, secondaryAnimation) => PlayerViewWidget(),
+        pageBuilder: (context, animation, secondaryAnimation) =>
+            PlayerViewWidget(),
         transitionsBuilder: (context, animation, secondaryAnimation, child) {
           var begin = Offset(0.0, 1.0);
           var end = Offset.zero;
           var curve = Curves.ease;
-          var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+          var tween =
+              Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
           var offsetAnimation = animation.drive(tween);
           return SlideTransition(
             position: offsetAnimation,
             child: child,
           );
-        }
-    );
+        });
   }
 
   Icon get playButtonIcon {
@@ -73,39 +62,42 @@ class _SmallPlayerWidgetState extends State<SmallPlayerWidget> {
               // TODO: Dit zou cleaner moeten kunnen, maar dit is kinda functioneel dus weet niet meteen hoe.
               onPressed: () {
                 Navigator.of(context).push(_onOpenPlayerPress());
-              }
-          ),
+              }),
           Container(
-              color: Color(0xff3F3C57),
-              padding:
-                  EdgeInsets.only(left: 20, right: 20, bottom: 10),
-              child: Row(
-                children: [
-                  Expanded(
-                    flex: 10,
-                    child: ClipRRect(
-                        borderRadius: BorderRadius.circular(8.0),
-                        child: Image(
-                          image: AssetImage('assets/becoming-wise.jpg'),
-                        )),
-                  ),
-                  SizedBox(width: 10),
-                  Expanded(
-                      flex: 80,
-                      child: Text(
-                        "Becoming wise episode #100: we are doing stuff",
-                        overflow: TextOverflow.ellipsis,
-                        style: Theme.of(context).textTheme.bodyText1,
-                      )),
-                  SizedBox(width: 10),
-                  Expanded(
+              color: Colors.transparent,
+              padding: EdgeInsets.only(left: 20, right: 20, bottom: 10),
+              child: Consumer<PlayerService>(
+                  builder: (context, playerService, child) {
+                return Row(
+                  children: [
+                    Expanded(
                       flex: 10,
-                      child: IconButton(
-                          color: Color(0xffEF476F),
-                          icon: playButtonIcon,
-                          onPressed: _onPlayButtonPress)),
-                ],
-              ))
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.circular(8.0),
+                          child: Image(
+                            image: AssetImage('assets/becoming-wise.jpg'),
+                          )),
+                    ),
+                    SizedBox(width: 10),
+                    Expanded(
+                        flex: 80,
+                        child: Text(
+                          "Becoming wise episode #100: we are doing stuff",
+                          overflow: TextOverflow.ellipsis,
+                          style: Theme.of(context).textTheme.bodyText1,
+                        )),
+                    SizedBox(width: 10),
+                    Expanded(
+                        flex: 10,
+                        child: IconButton(
+                            color: Color(0xffEF476F),
+                            icon: playButtonIcon,
+                            onPressed: () {
+                              _onPlayButtonPress(playerService);
+                            })),
+                  ],
+                );
+              }))
         ],
       ),
     );
