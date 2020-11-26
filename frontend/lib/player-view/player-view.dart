2144403718d840919a9143b60494967ad3174fc3
robinspitlijns <spiltijnsrobin@gmail.com>
2020-11-26 18:03:15 +0100

    play in player

--- frontend/lib/player-view/player-view.dart
+++ frontend/lib/player-view/player-view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:frontend/common/services/player-service.dart';
+import 'package:provider/provider.dart';
 
 class PlayerViewWidget extends StatefulWidget {
   @override
@@ -25,12 +27,17 @@ class _PlayerViewWidgetState extends State<PlayerViewWidget> {
         automaticallyImplyLeading: false,
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
+        child: 
+        Consumer<PlayerService>(
+          builder: (context, playerService, child) {
+            return ElevatedButton(
+              onPressed: () {
+                playerService.play("02f0123246c944e289ee2bb90804e41b");
+              },
+              child: Text("start"),
+            );
           },
-          child: Text("Hey"),
-        ),
+        )
       ),
     );
   }
