860d095fc06017d1ca29f643fec4b7d83f707da9
robinspitlijns <spiltijnsrobin@gmail.com>
2020-11-28 20:55:19 +0100

    load podcast back when app is closed

--- frontend/lib/common/services/player-service.dart
+++ frontend/lib/common/services/player-service.dart
@@ -1,20 +1,24 @@
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:swagger/api.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class PlayerService extends ChangeNotifier {
   // TODO: Permission to play audio a long time. (https://pub.dev/documentation/audioplayers/latest/)
   final AudioPlayer _audioPlayer = AudioPlayer();
   final _api = DefaultApi();
 
+  // TODO: Refactor naar bv episode data objecten?
   String _episodeTitle;
   Image _episodeImage;
   Image _episodeThumbnail;
   String _episodePublisher;
+  String _episodeAudio;
   Duration _episodeDuration;
   Duration _episodePosition;
   bool _isPlayingAudio;
-  bool _loadedPodcast;
+  bool _loadedPodcastAudio;
+  bool _loadedPodcastData;
 
   PlayerService() {
     _initializeState();
@@ -24,13 +28,31 @@ class PlayerService extends ChangeNotifier {
       notifyListeners();
     });
     _audioPlayer.onDurationChanged.listen((duration) {
-      // Possible performance bottleneck.
       _episodeDuration = duration;
       notifyListeners();
     });
+    notifyListeners();
+    _loadPersistedEpisode();
   }
 
-  void _initializeState() {
+  Future<void> _loadPersistedEpisode() async {
+    final prefs = await SharedPreferences.getInstance();
+    final episodeId = prefs.getString('episodeId') ?? "";
+    if (episodeId != "") {
+      Future<EpisodeFull> futureEpisodeFull = _api.getEpisode(episodeId);
+      futureEpisodeFull.then((episodeFull) {
+        _episodeTitle = episodeFull.title;
+        _episodeImage = Image.network(episodeFull.image);
+        _episodeThumbnail = Image.network(episodeFull.thumbnail);
+        _episodePublisher = episodeFull.podcast.publisher;
+        _episodeAudio = episodeFull.audio;
+        _loadedPodcastData = true;
+        notifyListeners();
+      });
+    }
+  }
+
+  Future<void> _initializeState() async {
     _episodeTitle = "No currently playing episode.";
     _episodeImage = Image.asset('assets/no-podcast.png');
     _episodeThumbnail = Image.asset('assets/no-podcast.png');
@@ -38,7 +60,9 @@ class PlayerService extends ChangeNotifier {
     _episodeDuration = Duration(seconds: 0);
     _episodePosition = Duration(seconds: 0);
     _isPlayingAudio = false;
-    _loadedPodcast = false;
+    _loadedPodcastData = false;
+    _loadedPodcastAudio = false;
+    notifyListeners();
   }
 
   bool get isPlayingAudio {
@@ -76,8 +100,14 @@ class PlayerService extends ChangeNotifier {
   }
 
   void resume() {
-    if (_loadedPodcast) {
-      _audioPlayer.resume();
+    if (_loadedPodcastData) {
+      if (_loadedPodcastAudio) {
+        _audioPlayer.resume();
+      } else {
+        _audioPlayer.play(_episodeAudio).then((result) => {
+              if (result == 1) {_loadedPodcastAudio = true}
+            });
+      }
       _isPlayingAudio = true;
       notifyListeners();
     }
@@ -99,18 +129,24 @@ class PlayerService extends ChangeNotifier {
     seek(_episodePosition - duration);
   }
 
+  Future<void> _persistPlayingEpisode(String episodeId) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString("episodeId", episodeId);
+  }
+
   void play(String episodeId) {
     Future<EpisodeFull> futureEpisodeFull = _api.getEpisode(episodeId);
     futureEpisodeFull.then((episodeFull) => {
-          _audioPlayer.play(episodeFull.audio).then((result) {
+          _audioPlayer.play(episodeFull.audio).then((result) async {
             if (result == 1) {
-              _loadedPodcast = true;
+              _loadedPodcastData = true;
               _isPlayingAudio = true;
               _episodeTitle = episodeFull.title;
               _episodeImage = Image.network(episodeFull.image);
               _episodeThumbnail = Image.network(episodeFull.thumbnail);
               _episodePublisher = episodeFull.podcast.publisher;
               notifyListeners();
+              await _persistPlayingEpisode(episodeId);
             }
           })
         });
