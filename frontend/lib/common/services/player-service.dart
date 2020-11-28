e956513b217a08e7427588c939f0a97f4472ea7a
robinspitlijns <spiltijnsrobin@gmail.com>
2020-11-28 19:38:03 +0100

    better initial player view, move start to search view

--- frontend/lib/common/services/player-service.dart
+++ frontend/lib/common/services/player-service.dart
@@ -3,7 +3,21 @@ import 'package:flutter/cupertino.dart';
 import 'package:swagger/api.dart';
 
 class PlayerService extends ChangeNotifier {
+  // TODO: Permission to play audio a long time. (https://pub.dev/documentation/audioplayers/latest/)
+  final AudioPlayer _audioPlayer = AudioPlayer();
+  final _api = DefaultApi();
+
+  String _episodeTitle;
+  Image _episodeImage;
+  Image _episodeThumbnail;
+  String _episodePublisher;
+  Duration _episodeDuration;
+  Duration _episodePosition;
+  bool _isPlayingAudio;
+  bool _loadedPodcast;
+
   PlayerService() {
+    _initializeState();
     _audioPlayer.onAudioPositionChanged.listen((position) {
       // Possible performance bottleneck.
       _episodePosition = position;
@@ -16,19 +30,16 @@ class PlayerService extends ChangeNotifier {
     });
   }
 
-  // TODO: Permission to play audio a long time. (https://pub.dev/documentation/audioplayers/latest/)
-  final AudioPlayer _audioPlayer = AudioPlayer();
-  final _api = DefaultApi();
-
-  String _episodeTitle = "No podcast playing at the moment";
-  Image _episodeImage = Image.network(
-      "https://image.freepik.com/free-vector/loading-icon_167801-436.jpg");
-  Image _episodeThumbnail = Image.network(
-      "https://image.freepik.com/free-vector/loading-icon_167801-436.jpg");
-  String _episodePublisher = "unknown";
-  Duration _episodeDuration = Duration(seconds: 0);
-  Duration _episodePosition = Duration(seconds: 0);
-  bool _isPlayingAudio = false;
+  void _initializeState() {
+    _episodeTitle = "No currently playing episode.";
+    _episodeImage = Image.asset('assets/no-podcast.png');
+    _episodeThumbnail = Image.asset('assets/no-podcast.png');
+    _episodePublisher = "Unavailable";
+    _episodeDuration = Duration(seconds: 0);
+    _episodePosition = Duration(seconds: 0);
+    _isPlayingAudio = false;
+    _loadedPodcast = false;
+  }
 
   bool get isPlayingAudio {
     return _isPlayingAudio;
@@ -65,9 +76,11 @@ class PlayerService extends ChangeNotifier {
   }
 
   void resume() {
-    _audioPlayer.resume();
-    _isPlayingAudio = true;
-    notifyListeners();
+    if (_loadedPodcast) {
+      _audioPlayer.resume();
+      _isPlayingAudio = true;
+      notifyListeners();
+    }
   }
 
   void addAudioPlayerDurationListener(Function(Duration) listener) {
@@ -91,6 +104,7 @@ class PlayerService extends ChangeNotifier {
     futureEpisodeFull.then((episodeFull) => {
           _audioPlayer.play(episodeFull.audio).then((result) {
             if (result == 1) {
+              _loadedPodcast = true;
               _isPlayingAudio = true;
               _episodeTitle = episodeFull.title;
               _episodeImage = Image.network(episodeFull.image);
