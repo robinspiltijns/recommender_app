1ce1c5cb8e4418a10276f994b8a2a643211ff74e
lottever <lotte.verheyden@gmail.com>
2020-11-29 21:56:38 +0100

    ListView deel voor episodes werkt

--- frontend/lib/search-results-view/search-results-view.dart
+++ frontend/lib/search-results-view/search-results-view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:frontend/search-results-view/components/episode-search-result-card.dart';
+import 'package:frontend/search-results-view/components/more-button.dart';
 import 'package:frontend/search-results-view/components/search-results-section.dart';
 import 'package:swagger/api.dart';
 
@@ -18,67 +21,70 @@ class SearchResultsViewWidget extends StatefulWidget {
 
 class _SearchResultsViewWidgetState extends State<SearchResultsViewWidget> {
 
-  Widget listView;
-
-  SearchResultsSection episodeResultsSection;
-  SearchResultsSection podcastResultsSection;
-
-  double episodeSectionHeight;
-  double podcastSectionHeight;
-
   @override
   void initState() {
     super.initState();
-    generateNewEpisodeResultsSection();
-    generateListView();
-  }
-
-  generateNewEpisodeResultsSection() async {
-    episodeResultsSection = SearchResultsSection(results: this.widget.episodes, t: ResultType.EPISODES, nbRes: 3, callbackFunction: this.callback, setHeightFunction: this.setHeight,);
-  }
-
-  ListView generateListView() {
-    print("height: " + episodeSectionHeight.toString());
-    return ListView(
-      scrollDirection: Axis.vertical,
-      children: [
-        Column(
-          children: [
-            episodeResultsSection,
-            //podcastResultsSecion,
-          ],
-        )
-      ],
-    );
-  }
-
-  void setHeight(ResultType type, double height) {
-    if (type == ResultType.EPISODES) {
-      this.episodeSectionHeight = height;
-    } else if (type == ResultType.PODCASTS) {
-      this.podcastSectionHeight = height;
-    }
-  }
-
-  void callback(SearchResultsSection section, ResultType type) {
-    if (type == ResultType.EPISODES) {
-      setState(() {
-        episodeResultsSection = section;
-      });
-    } else if (type == ResultType.PODCASTS) {
-      setState(() {
-        podcastResultsSection = section;
-      });
-    }
     setState(() {
-      listView = generateListView();
+      showingEpisodes.addAll(this.widget.episodes.getRange(present, present + perPage));
+      present = present + perPage;
     });
-    print(episodeResultsSection.nbResults);
   }
+
+  List<EpisodeSearchResult> showingEpisodes = [];
+
+  int perPage  = 3;
+  int present = 0;
+
+  Map<ResultType, String> titles = {
+    ResultType.EPISODES : "Episodes",
+    ResultType.PODCASTS : "Podcasts"
+  };
+
+
 
   @override
   Widget build(BuildContext context) {
-    return generateListView();
+    return Column(
+      children: [
+        Container(
+          height: SearchResultsSection.TITLE_PART_HEIGT,
+          alignment: AlignmentDirectional.centerStart,
+          margin: EdgeInsets.fromLTRB(10, 10, 0, 10),
+          child: Text(
+            titles[ResultType.EPISODES],
+            style: Theme.of(context).textTheme.headline2,
+          ),
+        ),
+        Flexible(
+          child: new ListView.builder(
+            itemCount: (present <= this.widget.episodes.length) ? showingEpisodes.length + 1 : showingEpisodes.length,
+            itemBuilder: (context, index) {
+              return (index == showingEpisodes.length ) ?
+                Container(
+                  height: SearchResultsSection.MORE_PART_HEIGT,
+                  child: FlatButton(
+                    child: MoreButton(),
+                    onPressed: () {
+                      setState(() {
+                        if((present + perPage)> this.widget.episodes.length) {
+                          showingEpisodes.addAll(
+                              this.widget.episodes.getRange(present, this.widget.episodes.length));
+                        } else {
+                          showingEpisodes.addAll(
+                              this.widget.episodes.getRange(present, present + perPage));
+                        }
+                        present = present + perPage;
+                    });
+                  },),
+                )
+              :
+              EpisodeSearchResultCard(
+                ep: this.showingEpisodes[index]);
+            },
+          ),
+        ),
+      ],
+    );
   }
 }
 
