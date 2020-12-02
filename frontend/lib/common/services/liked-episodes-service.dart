42c060281f31a27f8d3d89cc4314bf61cb4c8e53
joren.sjongers <joren.sjongers@icloud.com>
2020-12-02 16:30:51 +0100

    WIP

--- frontend/lib/common/services/liked-episodes-service.dart
+++ frontend/lib/common/services/liked-episodes-service.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:frontend/common/services/db-helper.dart';
+import 'package:frontend/object-model/episode.dart';
 import 'package:sqflite/sqflite.dart';
 
 class LikedEpisodesService extends ChangeNotifier {
@@ -14,7 +15,10 @@ class LikedEpisodesService extends ChangeNotifier {
     database = await DbHelper().db;
   }
 
-
-
+  Future<void> insert(Episode episode) async {
+    await database.query("""
+      
+    """);
+  }
 
 }
