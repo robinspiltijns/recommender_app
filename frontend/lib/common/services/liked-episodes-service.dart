ccb0ffb0501be685c7d1c73b49c5f33f6bfce653
joren.sjongers <joren.sjongers@icloud.com>
2020-12-04 11:32:28 +0100

    implement LikedEpisodesService

--- frontend/lib/common/services/liked-episodes-service.dart
+++ frontend/lib/common/services/liked-episodes-service.dart
@@ -1,11 +1,43 @@
 import 'package:flutter/cupertino.dart';
+import 'package:frontend/db-helper.dart';
 import 'package:frontend/object-model/episode.dart';
 import 'package:sqflite/sqflite.dart';
 
 class LikedEpisodesService extends ChangeNotifier {
-
-  Database database;
+  final Database database;
 
   LikedEpisodesService(this.database);
 
+  Future<List<Episode>> getLikedEpisodes() {
+    return database
+        .query(DatabaseHelper.likedEpisodesTable)
+        .then((list) =>
+            list.map((response) =>
+                Episode.fromDatabaseMap(response))
+            .toList()
+        );
+  }
+
+  Future<bool> isLiked(String id) {
+    return database
+        .query(DatabaseHelper.likedEpisodesTable,
+            columns: ["COUNT (*)"],
+            where: "$DatabaseHelper.idColumn = ?",
+            whereArgs: [id])
+        .then((result) => result.isNotEmpty);
+  }
+
+  void insertLikedEpisode(Episode episode) {
+    database
+        .insert(DatabaseHelper.likedEpisodesTable, episode.toMap())
+        .then((_) => notifyListeners());
+  }
+
+  void deleteLikedEpisode(String id) {
+    database
+        .delete(DatabaseHelper.likedEpisodesTable,
+            where: "$DatabaseHelper.idColumn = ?",
+            whereArgs: [id])
+        .then((_) => notifyListeners());
+  }
 }
