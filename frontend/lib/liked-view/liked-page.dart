b729e80159bf28c25a1d3dc28e0c02c677d5afce
joren.sjongers <joren.sjongers@icloud.com>
2020-12-11 16:39:58 +0100

    add navigator page for liked-view

---
+++ frontend/lib/liked-view/liked-page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/common/components/genre-details-view/genre-details-view.dart';
+import 'package:frontend/object-model/genre.dart';
+import 'liked-view.dart';
+
+
+class FeedPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Navigator(
+        onGenerateRoute: (RouteSettings settings) {
+          return MaterialPageRoute(
+              settings: settings,
+              builder: (BuildContext context) {
+                switch (settings.name) {
+                  case "/":
+                    return LikesWidget();
+                  case GenreDetails.routeName:
+                    final Genre genre = settings.arguments;
+                    return GenreDetails(genre);
+                }
+                return Container();
+              });
+        }
+    );
+  }
+}
