50a24c2724ae6aa037920151131be05f181a7605
joren.sjongers <joren.sjongers@icloud.com>
2020-12-12 14:06:42 +0100

    make search field clearable

--- frontend/lib/search-view/components/search-field.dart
+++ frontend/lib/search-view/components/search-field.dart
@@ -3,17 +3,26 @@ import 'package:flutter/material.dart';
 
 class SearchFieldWidget extends StatelessWidget {
 
+  var _controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return  TextField(
+      controller: _controller,
       onSubmitted: (value) {
         Navigator.pushNamed(context, "/search-results", arguments: value);
+        _controller.clear();
       },
       style: TextStyle(color: Colors.white),
       cursorColor: Colors.white,
       decoration: InputDecoration(
-        prefixIcon:
-        Icon(Icons.search, color: Colors.white.withOpacity(0.5)),
+        prefixIcon: Icon(Icons.search, color: Colors.white.withOpacity(0.5)),
+        suffixIcon: IconButton(
+          splashRadius: 25,
+          color: Colors.white.withOpacity(0.5),
+          onPressed: () => _controller.clear(),
+          icon: Icon(Icons.clear),
+        ),
         hintText: "Title, description, genre, ...",
         hintStyle: TextStyle(color: Colors.white.withOpacity(0.5)),
         filled: true,
