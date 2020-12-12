0c560d8baf30a44d9b237b3c1b797c62ef515d4d
joren.sjongers <joren.sjongers@icloud.com>
2020-12-12 12:59:21 +0100

    extend podcast object model

--- frontend/lib/object-model/podcast.dart
+++ frontend/lib/object-model/podcast.dart
@@ -2,25 +2,31 @@ import 'package:swagger/api.dart';
 
 class Podcast {
 
+  final String id;
   final String title;
   final String description;
   final String imageUrl;
+  final String producer;
 
-  Podcast(this.title, this.description, this.imageUrl);
+  Podcast(this.id, this.title, this.description, this.imageUrl, this.producer);
 
   static Podcast fromSearchResult(PodcastSearchResult result) {
     return Podcast(
+      result.id,
       result.titleOriginal,
       result.descriptionOriginal,
-      result.image
+      result.image,
+      result.publisherOriginal
     );
   }
 
-  static Podcast fromPodcastSipmle(PodcastSimple podcastSimple) {
+  static Podcast fromPodcastSimple(PodcastSimple podcastSimple) {
     return Podcast(
+      podcastSimple.id,
       podcastSimple.title,
       podcastSimple.description,
-      podcastSimple.image
+      podcastSimple.image,
+      podcastSimple.publisher
     );
   }
 
