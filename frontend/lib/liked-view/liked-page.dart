02bdd35f221ced88c382a8671fa3078dac5bed3d
joren.sjongers <joren.sjongers@icloud.com>
2020-12-12 12:59:36 +0100

    add routes to navigator

--- frontend/lib/liked-view/liked-page.dart
+++ frontend/lib/liked-view/liked-page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/common/components/genre-details-view/genre-details-view.dart';
+import 'package:frontend/common/components/podcast-details-view/podcast-details-view.dart';
 import 'package:frontend/object-model/genre.dart';
 import 'liked-view.dart';
 
@@ -19,8 +20,14 @@ class LikedPage extends StatelessWidget {
                   case GenreDetails.routeName:
                     Genre genre = settings.arguments;
                     return GenreDetails(genre);
+                  case PodcastDetails.routeName:
+                    String id = settings.arguments;
+                    return PodcastDetails(id);
                 }
-                return Container();
+                return Container(
+                  margin: EdgeInsets.only(top: 50),
+                  child: Text("RouteName ${settings.name} does not exist for likes view")
+                );
               });
         }
     );
