2040df498112743a3ca1d842801bbd45bfdc0082
joren.sjongers <joren.sjongers@icloud.com>
2020-12-12 12:57:43 +0100

    add route for podcast details page

--- frontend/lib/search-view/search-page.dart
+++ frontend/lib/search-view/search-page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:frontend/common/components/genre-details-view/genre-details-view.dart';
+import 'package:frontend/common/components/podcast-details-view/podcast-details-view.dart';
 import 'package:frontend/object-model/genre.dart';
 import 'package:frontend/search-results-view/search-results-view.dart';
 import 'search-view.dart';
@@ -26,8 +27,14 @@ class SearchPage extends StatelessWidget {
                   case GenreDetails.routeName:
                     Genre genre = settings.arguments;
                     return GenreDetails(genre);
+                  case PodcastDetails.routeName:
+                    String id = settings.arguments;
+                    return PodcastDetails(id);
                 }
-                return Container();
+                return Container(
+                    margin: EdgeInsets.only(top: 50),
+                    child: Text("RouteName ${settings.name} does not exist for search view")
+                );
               });
         }
     );
