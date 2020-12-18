eb42c148a6186bc894dc05ac193f1433f9983e37
robinspitlijns <spiltijnsrobin@gmail.com>
2020-12-18 15:09:52 +0100

    reformat

--- frontend/lib/common/components/bottom-controls.dart
+++ frontend/lib/common/components/bottom-controls.dart
@@ -38,50 +38,21 @@ class _BottomControlsWidgetState extends State<BottomControlsWidget> {
                   title: Text('Likes'),
                   activeColor: Color(0xffEF476F),
                   inactiveColor: Colors.white,
-                  textAlign: TextAlign.center
-
-              ),
+                  textAlign: TextAlign.center),
               BottomNavyBarItem(
                   icon: Icon(Icons.home),
                   title: Text('Feed'),
                   activeColor: Color(0xffEF476F),
                   inactiveColor: Colors.white,
-                  textAlign: TextAlign.center
-
-              ),
+                  textAlign: TextAlign.center),
               BottomNavyBarItem(
                   icon: Icon(Icons.search),
                   title: Text('Search'),
                   activeColor: Color(0xffEF476F),
                   inactiveColor: Colors.white,
-                  textAlign: TextAlign.center
-
-              ),
+                  textAlign: TextAlign.center),
             ],
           )
-          // BottomNavigationBar(
-          //   currentIndex: widget.selectedRouteIndex,
-          //   elevation: 0.0,
-          //   onTap: widget.routeChanged,
-          //   backgroundColor: Colors.transparent,
-          //   unselectedItemColor: Colors.white,
-          //   selectedItemColor: Color(0xffEF476F),
-          //   items: <BottomNavigationBarItem>[
-          //     BottomNavigationBarItem(
-          //       icon: Icon(Icons.account_circle),
-          //       label: 'Likes',
-          //     ),
-          //     BottomNavigationBarItem(
-          //       icon: Icon(Icons.home),
-          //       label: 'Feed',
-          //     ),
-          //     BottomNavigationBarItem(
-          //       icon: Icon(Icons.search),
-          //       label: 'Search',
-          //     ),
-          //   ],
-          //   type: BottomNavigationBarType.fixed,
-          // )
         ],
       ),
     );
