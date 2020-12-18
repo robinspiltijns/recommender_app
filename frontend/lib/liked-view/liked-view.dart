629561b0a40f1e33b4d14e57088c4dbbde304e51
joren.sjongers <joren.sjongers@icloud.com>
2020-12-18 15:34:04 +0100

    use most liked genres service in liked view

--- frontend/lib/liked-view/liked-view.dart
+++ frontend/lib/liked-view/liked-view.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:frontend/common/services/liked-episodes-service.dart';
 import 'package:frontend/liked-view/components/liked-genres.dart';
 import 'package:frontend/object-model/genre.dart';
 import 'package:frontend/liked-view/components/liked-episodes.dart';
+import 'package:provider/provider.dart';
 
 class LikesWidget extends StatefulWidget {
   @override
@@ -10,30 +12,44 @@ class LikesWidget extends StatefulWidget {
 }
 
 class LikesWidgetState extends State<LikesWidget> {
-  // mock data
-  List<Genre> likedGenres = [
-    new Genre(67, "Finance"),
-    new Genre(12, "Health"),
-    new Genre(33, "Technology"),
-    new Genre(56, "News"),
-  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Your Likes", style: Theme.of(context).textTheme.headline6),
-          backgroundColor: Colors.transparent,
-          shadowColor: Colors.transparent,
-          centerTitle: false,
-        ),
-        body: Container(
-            margin: const EdgeInsets.symmetric(horizontal: 8),
-            child: ListView(children: [
-              SizedBox(height: 30),
-              GenresWidget("Your most liked genres", likedGenres),
-              SizedBox(height: 30),
-              LikedEpisodesWidget(),
-            ])));
+      appBar: AppBar(
+        title: Text("Your Likes", style: Theme.of(context).textTheme.headline6),
+        backgroundColor: Colors.transparent,
+        shadowColor: Colors.transparent,
+        centerTitle: false,
+      ),
+      body: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8),
+        child: ListView(children: [
+          SizedBox(height: 30),
+          Consumer<LikedEpisodesService> (
+            builder: (context, likedEpisodesService, child) {
+              return FutureBuilder(
+                future: likedEpisodesService.getMostLikedGenres(),
+                builder: (context, AsyncSnapshot<List<Genre>> snapshot) {
+                  if (snapshot.hasData) {
+                    return GenresWidget(
+                      "Your most liked genres",
+                        snapshot.data
+                    );
+                  }
+                  if (snapshot.hasError) {
+                    print(snapshot.error);
+                  }
+                  return CircularProgressIndicator();
+                }
+              );
+            }
+          ),
+          SizedBox(height: 30),
+          LikedEpisodesWidget(),
+        ]
+       )
+      )
+    );
   }
 }
