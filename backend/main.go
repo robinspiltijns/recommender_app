bb9f5ec9d2d0f1e23c7012f8ece54feb3a9e2811
joren.sjongers <joren.sjongers@icloud.com>
2020-12-19 13:04:08 +0100

    init database

--- backend/main.go
+++ backend/main.go
@@ -10,8 +10,10 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 
 	// WARNING!
 	// Change this to a fully-qualified import path
@@ -20,13 +22,51 @@ import (
 	//
 	//    sw "github.com/myname/myrepo/go"
 	//
+	_ "github.com/mattn/go-sqlite3"
 	sw "github.com/robinspiltijns/recommender_app/backend/go"
 )
 
+const dbFileName = "logging-database.db"
+
 func main() {
+
+	os.Remove(dbFileName)
+
+	log.Println("Creating" + dbFileName + "...")
+	file, err := os.Create(dbFileName) // Create SQLite file
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	file.Close()
+	log.Println("database file created")
+
+	db, err := sql.Open("sqlite3", "./"+dbFileName)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer db.Close()
+
+	createTable(db)
+
 	log.Printf("Server started")
 
 	router := sw.NewRouter()
 
 	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func createTable(db *sql.DB) {
+	createTableQuery := `CREATE TABLE timing (
+		user_id TEXT PRIMARY KEY NOT NULL,
+		app_version TEXT,		
+		start_time TEXT,
+		stop_time TEXT
+	  )`
+
+	log.Println("Create timing table...")
+	if _, err := db.Exec(createTableQuery); err != nil {
+		log.Fatal(err.Error())
+	}
+
+	log.Println("timing table created")
 }
